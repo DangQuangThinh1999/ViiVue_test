0b43ce93fd08c2426597baebf8220ddc330d79af
ĐẶNG QUANG THỊNH <93868132+DangQuangThinh1999@users.noreply.github.com>
2023-06-12 22:01:45 +0700

    feat: create animation text and number (#2)

--- app.js
+++ app.js
@@ -87,17 +87,34 @@ function dataImage(props) {
   return `
          
               ${props
-                .map(
-                  (item) =>
-                    `  
+                .map((item, index) => {
+                  const txt = item.content;
+                  let numb = txt.match(/\d/g).join(""); // filter numbers
+                  let text = "";
+                  if (index === 0) {
+                    text = txt
+                      .split("")
+                      .filter((value, index) => index > 3)
+                      .join("");
+                    numb = Math.floor(numb / 10);
+                  }
+
+                  text = txt.replace(numb, "");
+
+                  console.log(numb, text);
+                  return `  
                 <div class="wrapper_circle" style="left:${item.styles.left};top:${item.styles.top}">
                     <div class="circle" style="height:${item.styles.height};width:${item.styles.width}">
-                       <div class="content"> 
-                            <h1>${item.content}</h1>
+                       <div class="content "> 
+                            <h1>   
+                            <span class="nbr">${numb}</span>     
+                            <span>${text}</span>               
+                            </h1>
+                            
                         </div>
                     </div>
-                </div>`
-                )
+                </div>`;
+                })
                 .join("")}
           `;
 }
@@ -106,3 +123,118 @@ let appText = document.querySelector("#data_text");
 appText.innerHTML = dataText(data);
 let appImage = document.querySelector("#data_images");
 appImage.innerHTML = dataImage(circles);
+
+const textValue = document.querySelectorAll(".fancy");
+
+textValue.forEach((text, index) => {
+  const strText = text.textContent;
+  const splitText = strText.split("");
+
+  text.textContent = "";
+  for (let i = 0; i < splitText.length; i++) {
+    text.innerHTML +=
+      `<span class=${index === 1 ? "blue" : "black"}>` +
+      splitText[i] +
+      "</span>";
+  }
+
+  let char = 0;
+  let timer = setInterval(onTick, 15);
+
+  function onTick() {
+    const span = text.querySelectorAll("span")[char];
+    span.classList.add("fade");
+    char++;
+    if (char === splitText.length) {
+      complete();
+      return;
+    }
+  }
+
+  text.addEventListener("OnHover", onTick());
+  function complete() {
+    clearInterval(timer);
+    timer = null;
+  }
+});
+// -----------------------------------------------------------------
+
+let words = document.querySelectorAll(".word");
+
+let wordArray = []; //will store arrays of letters for each word
+let currentWord = 0; //will store the index of the currently displayed word;
+words[currentWord].style.opacity = 1;
+const splitLetters = (word) => {
+  let content = word.innerText;
+  word.innerText = "";
+  let letters = [];
+  for (let i = 0; i < content.length; i++) {
+    let letter = document.createElement("span");
+    letter.className = "letter";
+    letter.innerText = content.charAt(i);
+    word.appendChild(letter);
+    letters.push(letter);
+  }
+  wordArray.push(letters);
+};
+for (let i = 0; i < words.length; i++) {
+  splitLetters(words[i]);
+}
+const animateLetterOut = (cw, i) => {
+  setTimeout(function () {
+    cw[i].className = "letter out";
+  }, i * 80);
+};
+
+const animateLetterIn = (nw, i) => {
+  setTimeout(function () {
+    nw[i].className = "letter in";
+  }, 340 + i * 80); // delay of 340, so that new letters ("design") start falling down once the first animation is completed.
+};
+
+const changeWord = () => {
+  let cw = wordArray[currentWord]; // wordArray[0] gives us: [c,o,d,e]
+  let nw =
+    currentWord == words.length - 1 ? wordArray[0] : wordArray[currentWord + 1]; // evals to wordArray[1] and gives us: [d,e,s,i,g,n]
+
+  for (let i = 0; i < cw.length; i++) {
+    animateLetterOut(cw, i); // called for each letter of [c,o,d,e] with different i values, so we have a delay between each letter when they fade out.
+  }
+
+  for (let i = 0; i < nw.length; i++) {
+    //for each letter inside [d,e,s,i,g,n]
+    nw[i].className = "letter behind"; //we set initial position to the top
+    nw[0].parentElement.style.opacity = 1; //we set the opacity to 1, but currently invisible due to overlow hidden.
+    animateLetterIn(nw, i); //animates each letter as if they fall down from top.
+  }
+  //update currentWord index.
+  currentWord = currentWord == wordArray.length - 1 ? 0 : currentWord + 1;
+};
+changeWord(); //initial call
+setInterval(changeWord, 5000);
+
+var speed = 10;
+
+/* Call this function with a string containing the ID name to
+ * the element containing the number you want to do a count animation on.*/
+function incEltNbr(id) {
+  elt = document.querySelectorAll(id);
+  elt.forEach((item) => {
+    endNbr = Number(item.innerHTML);
+    incNbrRec(0, endNbr, item);
+  });
+}
+
+/*A recursive function to increase the number.*/
+function incNbrRec(i, endNbr, elt) {
+  console.log(endNbr);
+  let count = endNbr >= 100 && endNbr % 100 === 0 ? 10 : 1;
+  if (i <= endNbr) {
+    elt.innerHTML = i;
+    setTimeout(function () {
+      //Delay a bit before calling the function again.
+      incNbrRec(i + count, endNbr, elt);
+    }, speed);
+  }
+}
+incEltNbr(".nbr");
